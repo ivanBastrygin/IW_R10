661ad256b4be62bd9bac2b58691db3c51abb089d
ivanBastrygin <96539193+ivanBastrygin@users.noreply.github.com>
2022-12-22 11:56:24 +0300

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	
+	
+<form action="script.php" method="post">
+<input type="text" name="x" placeholder="X">
+<input type="text" name="a"placeholder="A">
+<input type="text" name="b"placeholder="B">
+<input type="text" name="c"placeholder="C">
+
+<button>Вычислить</button>
+</form>
+
+
+</body>
+</html>
